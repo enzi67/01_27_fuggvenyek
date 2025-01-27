7382010637abb89616044f841c78b1b14883fc94
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-01-27 08:34:31 +0100

    feladat

---
+++ 01_eljaras.py
@@ -0,0 +1,19 @@
+def shiii():
+    print("meow")
+
+shiii()
+
+
+def koszont_ket_nevvel(nev1, nev2):
+      print('Szia '+ nev1 + ', ' + nev2 +'!\nÜdv a fedélzeten!')
+
+koszont_ket_nevvel('Nóra', 'Ádám')
+
+
+def koszont_ket_nevvel(nev1, nev2):
+      print('Szia '+ nev1 + ', ' + nev2 +'!\nÜdv a fedélzeten!')
+
+nev1 = 'Nóra'
+nev2 = 'Ádám'
+koszont_ket_nevvel(nev2, nev1)
+    
