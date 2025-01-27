a551dfa8ed43fde793b978877d0ddbde6b928b82
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-01-27 09:37:42 +0100

    num

--- 1_2_feladat.py
+++ 1_2_feladat.py
@@ -3,15 +3,15 @@
 Írj eljárást, amely paraméterül kapott számról eldönti, és a képernyőre kiírja,
 hogy negatív, pozitív vagy nulla-e!
 """
-def elso_feladat():
+def elso_feladat(num):
 
-    if szam > 0:
+    if num > 0:
         print("Pozitív")
-    elif szam < 0:
+    elif num < 0:
         print("Negatív")
     else:
         print("Nulla vagy nincs megoldás.")
         
-szam = int(input("Adj meg egy számot: "))
+num = int(input("Adj meg egy számot: "))
 
-elso_feladat()
+elso_feladat(num)
