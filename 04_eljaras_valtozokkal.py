25f8ea614f477774bb17e6a19b30712841d8a380
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-01-27 08:50:50 +0100

    javítás

--- 04_eljaras_valtozokkal.py
+++ 04_eljaras_valtozokkal.py
@@ -6,13 +6,14 @@ def kepernyore_ir():
     lokalis_valtozo = 'alma'
     print(lokalis_valtozo)
     print(globalis_valtozo)
-  
+    return lokalis_valtozo
   
 globalis_valtozo = 'gyümölcs'
 kepernyore_ir()
+print(kepernyore_ir())
   
 print(globalis_valtozo)
 # a lokalis_valtozo az eljáráson KÍVŰL nem elérhető !!!
-print(lokalis_valtozo) # hibát eredményez !!!
+# print(lokalis_valtozo) # hibát eredményez !!!
     
     
