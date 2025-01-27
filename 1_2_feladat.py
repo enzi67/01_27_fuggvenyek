e1b3b015f82581079ca54a186ea0ad36ad26433d
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2025-01-27 09:28:43 +0100

    update

--- 1_2_feladat.py
+++ 1_2_feladat.py
@@ -5,13 +5,12 @@ hogy negatív, pozitív vagy nulla-e!
 """
 def elso_feladat():
 
-    szam = int(input("Adj meg egy számot: "))
-
     if szam > 0:
         print("Pozitiv")
     elif szam < 0:
         print("Negativ")
     else:
         print("Nulla avyg nincs megoldás.")
+szam = int(input("Adj meg egy számot: "))
 
 elso_feladat()
